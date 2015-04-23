8a8f403334be271e20be71fbfa4d26456f6a9041
bumb0ks <bum.boks@hotmail.com>
2015-04-23 11:16:07 +0300

    anoton bugs

--- laba/Form3.cs
+++ laba/Form3.cs
@@ -49,6 +49,7 @@ namespace laba
         }
         //
         //
+        //
         private void label1_Click(object sender, EventArgs e)
         {
             Program.job = "Акушерство и гинекология";
